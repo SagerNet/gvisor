493c38f38a782b6b3e139b4daaa0eab7ca33d6cb
世界 <i@sekai.icu>
2023-12-09 18:12:16 +0800

    fdbased: Enable GSO for TUN

--- pkg/tcpip/link/fdbased/endpoint.go
+++ pkg/tcpip/link/fdbased/endpoint.go
@@ -320,15 +320,13 @@ func New(opts *Options) (stack.LinkEndpoint, error) {
 			return nil, err
 		}
 		e.fds = append(e.fds, fdInfo{fd: fd, isSocket: isSocket})
-		if isSocket {
-			if opts.GSOMaxSize != 0 {
-				if opts.GVisorGSOEnabled {
-					e.gsoKind = stack.GVisorGSOSupported
-				} else {
-					e.gsoKind = stack.HostGSOSupported
-				}
-				e.gsoMaxSize = opts.GSOMaxSize
+		if opts.GSOMaxSize != 0 {
+			if opts.GVisorGSOEnabled {
+				e.gsoKind = stack.GVisorGSOSupported
+			} else {
+				e.gsoKind = stack.HostGSOSupported
 			}
+			e.gsoMaxSize = opts.GSOMaxSize
 		}
 		if opts.ProcessorsPerChannel == 0 {
 			opts.ProcessorsPerChannel = max(1, runtime.GOMAXPROCS(0)/len(opts.FDs))
