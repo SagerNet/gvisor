12e09180d768e522b3d7a70237a42ffebf8e3077
世界 <i@sekai.icu>
2024-10-19 09:32:50 +0800

    Export packet from TCP forwarder request

--- pkg/tcpip/transport/tcp/forwarder.go
+++ pkg/tcpip/transport/tcp/forwarder.go
@@ -170,3 +170,7 @@ func (r *ForwarderRequest) CreateEndpoint(queue *waiter.Queue) (tcpip.Endpoint, 
 
 	return ep, nil
 }
+
+func (r *ForwarderRequest) Packet() *stack.PacketBuffer {
+	return r.segment.pkt
+}
