365e9ffd75c6e5385a835f0f74da2460c7d15754
世界 <i@sekai.icu>
2024-10-21 11:24:25 +0800

    Export packet from UDP forwarder request

--- pkg/tcpip/transport/udp/forwarder.go
+++ pkg/tcpip/transport/udp/forwarder.go
@@ -103,3 +103,7 @@ func (r *ForwarderRequest) CreateEndpoint(queue *waiter.Queue) (tcpip.Endpoint, 
 
 	return ep, nil
 }
+
+func (r *ForwarderRequest) Packet() *stack.PacketBuffer {
+	return r.pkt
+}
